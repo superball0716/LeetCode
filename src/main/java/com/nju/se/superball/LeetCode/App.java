390dc455b2eaee052b26589307a5d54a0f211502
管梦秋 <mf1732033@smail.nju.edu.cn>
2017-12-25 21:18:00 +0800

    init

---
+++ src/main/java/com/nju/se/superball/LeetCode/App.java
@@ -0,0 +1,13 @@
+package com.nju.se.superball.LeetCode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
