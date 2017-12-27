2f09f7591f1eb58d836c8e5048792de50f4ace7c
管梦秋 <mf1732033@smail.nju.edu.cn>
2017-12-27 12:59:22 +0800

    init TreeNode

---
+++ src/main/java/Tree/TreeNode.java
@@ -0,0 +1,17 @@
+package Tree;
+
+/**
+ * Definition for a binary tree node.
+ * 
+ * @author superball
+ * @time 2017年12月27日下午12:57:27
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
