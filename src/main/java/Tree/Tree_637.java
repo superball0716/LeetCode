790c1d6dc50a730ea97f314fe42519e560657613
管梦秋 <634880846@qq.com>
2017-12-27 13:05:51 +0800

    create 637

---
+++ src/main/java/Tree/Tree_637.java
@@ -0,0 +1,11 @@
+package Tree;
+
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class Tree_637 {
+    public List<Double> averageOfLevels(TreeNode root) {
+        return null;
+    }
+}
