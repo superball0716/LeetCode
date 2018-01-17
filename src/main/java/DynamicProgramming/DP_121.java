b1c2d82aaf71e27e4fe2453ae6e7b03757c1c2f7
管梦秋 <634880846@qq.com>
2018-01-17 17:26:30 +0800

    DP_121
    Say you have an array for which the ith element is the price of a given
    stock on day i.
    
    If you were only permitted to complete at most one transaction (ie, buy
    one and sell one share of the stock), design an algorithm to find the
    maximum profit.

---
+++ src/main/java/DynamicProgramming/DP_121.java
@@ -0,0 +1,41 @@
+package DynamicProgramming;
+
+/**
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+ * @author superball
+ * @time 2018年1月17日下午5:19:27
+ */
+public class DP_121 {
+
+	public static void main(String[] args) {
+		DP_121 main = new DP_121();
+		System.out.println(main.maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+	}
+
+	public int maxProfit(int[] prices) {
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
+		int min = prices[0];
+		int max = 0;
+		for (int i = 1; i < prices.length; i++) {
+			min = Math.min(min, prices[i]);
+			max = Math.max(max, prices[i] - min);
+		}
+		return max;
+	}
+
+}
